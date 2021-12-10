8e400a55894246b7bfd2bae2a3d2b5554f9d5f88
Sergii Levchenko <levchenko88@gmail.com>
2021-12-10 16:16:52 +0000

    Added tomcat install

--- tomcat/install_tomcat.sh
+++ tomcat/install_tomcat.sh
@@ -1,21 +1,105 @@
-#!/bin/bas
+#!/bin/bash
 
-nstall_tomcat () {
-sudo apt install default-jdk
+install_default_jdk() {
+    sudo apt install default-jdk &> $log_path/tmp.log
+if [ $? -eq 0 ];
+    then
+        info "nstall_default_jdk complete"
+    else
+        tail -n20 $log_path/tmp.log
+        error "nstall_default_jdk failed"
+    exit 1
+fi
+}
 
-sudo useradd -m -U -d /opt/tomcat -s /bin/false tomcat
-cd /tmp
-wget https://archive.apache.org/dist/tomcat/tomcat-8/v8.0.20/bin/apache-tomcat-8.0.20.zip
-#   sudo apt-add-repository "deb https://repository.apache.org/content/repositories/snapshots/org/apache/tomcat/tomcat-catalina/8.0-SNAPSHOT/tomcat-catalina-8.0-20140804.190218-19.jar"
+add_user () {
+    sudo useradd -m -U -d /opt/tomcat -s /bin/false tomcat &> $log_path/tmp.log
+if [ $? -eq 0 ];
+    then
+        info "add_user complete"
+    else
+        tail -n20 $log_path/tmp.log
+        error "add_user failed"
+    exit 1
+fi
+}
 
-unzip apache-tomcat-8.0.20.zip
-sudo mkdir -p /opt/tomcat
-sudo mv apache-tomcat-8.0.20 /opt/tomcat/
-sudo ln -s /opt/tomcat/apache-tomcat-8.0.20 /opt/tomcat/latest
-sudo chown -R tomcat: /opt/tomcat
-sudo sh -c 'chmod +x /opt/tomcat/latest/bin/*.sh'
+get_tomcat () {
+    cd /tmp
+    wget https://archive.apache.org/dist/tomcat/tomcat-8/v8.0.20/bin/apache-tomcat-8.0.20.zip &> $log_path/tmp.log
+if [ $? -eq 0 ];
+    then
+        info "get_tomcat complete"
+    else
+        tail -n20 $log_path/tmp.log
+        error "get_tomcat failed"
+    exit 1
+fi
+}
 
-cat << EOF > /etc/systemd/system/tomcat.service
+unzip_tomcat () {
+    unzip apache-tomcat-8.0.20.zip &> $log_path/tmp.log
+if [ $? -eq 0 ];
+    then
+        info "unzip_tomcat complete"
+    else
+        tail -n20 $log_path/tmp.log
+        error "unzip_tomcat failed"
+    exit 1
+fi
+}
+
+move_tomcat () {
+    sudo mkdir -p /opt/tomcat
+    sudo mv apache-tomcat-8.0.20/*  /opt/tomcat/ &> $log_path/tmp.log
+if [ $? -eq 0 ];
+    then
+        info "move_tomcat complete"
+    else
+        tail -n20 $log_path/tmp.log
+        error "move_tomcat failed"
+    exit 1
+fi
+}
+
+hard_link () {
+    sudo ln -s /opt/tomcat/apache-tomcat-8.0.20 /opt/tomcat/latest &> $log_path/tmp.log
+if [ $? -eq 0 ];
+    then
+        info "hard_link complete"
+    else
+        tail -n20 $log_path/tmp.log
+        error "hard_link failed"
+    exit 1
+fi
+}
+
+add_owner () {
+    sudo chown -R tomcat.tomcat /opt/tomcat &> $log_path/tmp.log
+if [ $? -eq 0 ];
+    then
+        info "add_owner complete"
+    else
+        tail -n20 $log_path/tmp.log
+        error "add_owner failed"
+    exit 1
+fi
+}
+
+add_permission () {
+    sudo sh -c 'chmod +x /opt/tomcat/latest/bin/*.sh' &> $log_path/tmp.log
+if [ $? -eq 0 ];
+    then
+        info "add_permission complete"
+    else
+        tail -n20 $log_path/tmp.log
+        error "add_permission failed"
+    exit 1
+fi
+}
+
+add_service () {
+    cat << EOF > /etc/systemd/system/tomcat.service &> $log_path/tmp.log
 [Unit]
 Description=Tomcat 8.0.20 servlet container
 After=network.target
@@ -40,10 +124,106 @@ ExecStop=/opt/tomcat/latest/bin/shutdown.sh
 [Install]
 WantedBy=multi-user.target
 EOF
-
-sudo systemctl daemon-reload
-sudo systemctl start tomcat
-sudo systemctl stop tomcatw
-udo systemctl enable tomcat
-sudo ufw allow 8080
+if [ $? -eq 0 ];
+    then
+        info "add_service complete"
+    else
+        tail -n20 $log_path/tmp.log
+        error "add_service failed"
+    exit 1
+fi
 }
+
+daemon_reload () {
+sudo systemctl daemon-reload
+if [ $? -eq 0 ];
+    then
+        info "add_service complete"
+    else
+        tail -n20 $log_path/tmp.log
+        error "add_service failed"
+    exit 1
+fi
+}
+
+start_tomcat () {
+sudo systemctl start tomcat
+if [ $? -eq 0 ];
+    then
+        info "start_tomcat complete"
+    else
+        tail -n20 $log_path/tmp.log
+        error "start_tomcat failed"
+    exit 1
+fi
+}
+
+stop_tomcat () {
+sudo systemctl stop tomcat
+if [ $? -eq 0 ];
+    then
+        info "stop_tomcat complete"
+    else
+        tail -n20 $log_path/tmp.log
+        error "stop_tomcat failed"
+    exit 1
+fi
+}
+
+enable_tomcat () {
+sudo systemctl enable tomcat
+if [ $? -eq 0 ];
+    then
+        info "enable_tomcat complete"
+    else
+        tail -n20 $log_path/tmp.log
+        error "enable_tomcat failed"
+    exit 1
+fi
+}
+
+allow_8080 () {
+sudo ufw allow 8080
+if [ $? -eq 0 ];
+    then
+        info "allow_8080 complete"
+    else
+        tail -n20 $log_path/tmp.log
+        error "allow_8080 failed"
+    exit 1
+fi
+}
+
+main. () {
+
+install_default_jdk
+
+add_user
+
+get_tomcat
+
+unzip_tomcat
+
+move_tomcat
+
+hard_link
+
+add_owner
+
+add_permission
+
+add_service
+
+daemon_reload
+
+start_tomcat
+sleep 5
+
+stop_tomcat
+
+enable_tomcat
+
+allow_8080
+}
+
+main
